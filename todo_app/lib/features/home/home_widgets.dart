70a7fb87ac653ad5832b3f1cee773d397636126e
Umer Farooq Jillani <umerfarooqjillani92@gmail.com>
2025-09-15 17:10:18 +0500

    Updated

--- todo_app/lib/features/home/home_widgets.dart
+++ todo_app/lib/features/home/home_widgets.dart
@@ -3,8 +3,6 @@ import 'package:flutter/material.dart';
 
 import 'package:todo_app/app/constants/strings.dart';
 
-
-
 class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
   const MyAppBar({super.key});
 
