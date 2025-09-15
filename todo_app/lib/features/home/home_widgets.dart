191031386a219d115e5f5d0c3d4484ab6ec04b8c
Umer Farooq Jillani <umerfarooqjillani92@gmail.com>
2025-09-15 17:23:44 +0500

    add the buttons in appbar

--- todo_app/lib/features/home/home_widgets.dart
+++ todo_app/lib/features/home/home_widgets.dart
@@ -3,16 +3,26 @@ import 'package:flutter/material.dart';
 
 import 'package:todo_app/app/constants/strings.dart';
 
+// ignore: must_be_immutable
 class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
-  const MyAppBar({super.key});
+  MyAppBar({super.key});
+  bool isdark = true;
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
       title: Text(appTitle, style: Theme.of(context).textTheme.titleLarge),
+      actions: [
+        Row(
+          children: [
+            IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+            Switch(value: isdark, onChanged: (value) {}, ),
+          ],
+        ),
+      ],
     );
   }
 
   @override
-  Size get preferredSize => Size.fromHeight(kToolbarHeight); 
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
 }
